34a2811aea4788d41e032500c7fb45c2e50b7e02
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-07-13 23:17:13 +1000

    pwm

--- promin-dev/analogWrite/analogWrite.ino
+++ promin-dev/analogWrite/analogWrite.ino
@@ -1,20 +1,23 @@
-
 void setup() 
 {
 pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+Serial.begin(115200);
+
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
    TCCR1B = _BV(CS10);
 
   OCR1A = 180;
   OCR1B = 50;
-//  analogWrite(9,50);
-//  analogWrite(10,180);
-Serial.begin(115200);
 }
 
 void loop() 
 {
-Serial.println((OCR1A));
+int  val = analogRead(A0);
 
+  Serial.println((val));
 }

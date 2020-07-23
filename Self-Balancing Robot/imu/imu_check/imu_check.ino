c7b21e36d53f3e1b35f5d4f0652c3f7183475f0b
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-07-23 17:44:34 +1000

    timer interrupt and title angle estimation

--- Self-Balancing Robot/imu/imu_check/imu_check.ino
+++ Self-Balancing Robot/imu/imu_check/imu_check.ino
@@ -6,7 +6,25 @@ double roll = 0.0;
 float alpha = 0.9; 
 float dt= 0.01; 
 float GxConversionFactor = 245 / (65535/2); 
-int angleest;
+int angleest = 0;
+
+const int arrayLength = 5;
+int xAccelArray[arrayLength] = {0,0,0,0,0};
+char xAccelIndex = 0;
+long Ax = 0;
+int yAccelArray[arrayLength] = {0,0,0,0,0};
+char yAccelIndex = 0;
+long Ay = 0;
+int zAccelArray[arrayLength] = {0,0,0,0,0};
+char zAccelIndex = 0;
+long Az = 0;
+
+int xGyroArray[arrayLength] = {0,0,0,0,0};
+char xGyroIndex = 0;
+int Gx = 0;
+
+long sumArray(int a[], int);
+
 void setup(){
 
 Wire.begin();
@@ -29,18 +47,48 @@ Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
 GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
 GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
 GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-Serial.print("AcX = "); Serial.print(AcX);
-Serial.print(" | AcY = "); Serial.print(AcY);
-Serial.print(" | AcZ = "); Serial.print(AcZ);
-Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //equation for temperature in degrees C from datasheet
-Serial.print(" | GyX = "); Serial.print(GyX);
-Serial.print(" | GyY = "); Serial.print(GyY);
-Serial.print(" | GyZ = "); Serial.println(GyZ);
+//Serial.print("AcX = "); Serial.print(AcX);
+//Serial.print(" | AcY = "); Serial.print(AcY);
+//Serial.print(" | AcZ = "); Serial.print(AcZ);
+//Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //equation for temperature in degrees C from datasheet
+//Serial.print(" | GyX = "); Serial.print(GyX);
+//Serial.print(" | GyY = "); Serial.print(GyY);
+//Serial.print(" | GyZ = "); Serial.println(GyZ);
 
-roll = atan2(((double) AcX), (double) sqrt(AcY*AcY + AcZ*AcZ))*57.3;
-GyX = GyX*GxConversionFactor;
+xAccelArray[xAccelIndex] = AcX;
+xAccelIndex = (xAccelIndex+1)%arrayLength;
+
+yAccelArray[yAccelIndex] = AcY;
+yAccelIndex = (yAccelIndex+1)%arrayLength;
+
+zAccelArray[zAccelIndex] = AcZ;
+zAccelIndex = (zAccelIndex+1)%arrayLength;
+
+xGyroArray[xGyroIndex] = GyX;
+xGyroIndex = (xGyroIndex+1)%arrayLength;
+
+// Estimate Angle
+Ax = sumArray(xAccelArray, arrayLength)/arrayLength;
+Ay = sumArray(yAccelArray, arrayLength)/arrayLength;
+Az = sumArray(zAccelArray, arrayLength)/arrayLength;
+
+Gx = sumArray(xGyroArray,arrayLength)/arrayLength;
+
+roll = atan2(((double) Ax), (double) sqrt(Ay*Ay + Az*Az))*57.3;
+GyX = Gx*GxConversionFactor;
 angleest = round(alpha*(angleest + dt*GyX) + (1-alpha)*roll); 
 Serial.print("angleest\t"); Serial.println(angleest);
 
 delay(100);
+}
+
+long sumArray(int a[], int num_elements)
+{
+   int i;
+   long sum=0;
+   for (i=0; i<num_elements; i++)
+   {
+     sum = sum + a[i];
+   }
+   return(sum);
 }
---
+++ Self-Balancing Robot/imu/interrupt_imu_jul22a/interrupt_imu_jul22a.ino
@@ -0,0 +1,43 @@
+// 23/07/20
+// Timer interrupt on timer 2, 100 Hz 
+
+int pin = 11;
+boolean toggle2 = 0;
+
+void setup() {
+
+cli();//stop interrupts
+
+pinMode(11,OUTPUT); 
+//set timer2 interrupt at 8kHz
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 155;// = (16*10^6) / (8000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21)|(1 << CS20)|(1 << CS22);   
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+
+
+sei();//allow interrupts
+}
+
+ISR(TIMER2_COMPA_vect){//timer1 interrupt 8kHz toggles pin 9
+//generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
+//  if (toggle2){
+//    digitalWrite(pin,HIGH);
+//    toggle2 = 0;
+//  }
+//  else{
+//    digitalWrite(pin,LOW);
+//    toggle2 = 1;
+//  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+//digitalWrite(11,HIGH);
+}

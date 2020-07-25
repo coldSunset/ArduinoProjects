a485475c895d8aa4810b0d3aab07a07f9c7aa191
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-07-25 19:59:39 +1000

    integration

--- Self-Balancing Robot/iteration_1/integration_jul25a/integration_jul25a.ino
+++ Self-Balancing Robot/iteration_1/integration_jul25a/integration_jul25a.ino
@@ -1,7 +1,44 @@
 #include<Wire.h>
+// includes PWM 
 
 bool imu_flag = false; 
 
+// MOTOR PIN ASSIGNMENTS 
+const int ENA = 9; // PWM signal on promini (remove jumper) 
+const int ENB = 10; // second PWM signal on promini
+
+const int MOT_IN1 = 3; //digital pins for direction control 
+const int MOT_IN2 = 4;
+
+const int MOT_IN3 = 5;
+const int MOT_IN4 = 6;
+
+int MOT_SPEED1 = 0; // speed of motors 
+int MOT_SPEED2 = 0; 
+
+//structure defintions for motor 
+
+struct Motor
+{
+  const int enablePin; 
+  const int inputPin1; 
+  const int inputPin2; 
+  int motorSpeed;      
+
+}; 
+
+//Function prototypes
+void setup_Motor(struct Motor thisMotor);
+void brakeMotor(struct Motor thisMotor); 
+void goForward(struct Motor thisMotor); 
+void goBackward(struct Motor thisMotor); 
+
+//strcuture initialisations 
+struct Motor motorA = {ENA, MOT_IN1, MOT_IN2, MOT_SPEED1}; 
+struct Motor motorB = {ENB, MOT_IN3, MOT_IN4, MOT_SPEED2}; 
+  
+
+// IMU 
 const int MPU_addr=0x68; // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 double roll = 0.0;
@@ -33,12 +70,13 @@ void setup()
 {
   //cli(); 
   pinMode(13, OUTPUT);
-  
+  setup_Motor(motorA); 
+  setup_Motor(motorB);
   TCCR2A = 0;// set entire TCCR2A register to 0
   TCCR2B = 0;// same for TCCR2B
   TCNT2  = 0;//initialize counter value to 0
   // set compare match register for 100 Hz increments
-  OCR2A = 256;// = (16*10^6) / (100*1024) - 1 (must be <256)
+  OCR2A = 155;// = (16*10^6) / (100*1024) - 1 (must be <256)
   // turn on CTC mode
   TCCR2A |= (1 << WGM21);
   // Set CS21 bit for 1024 prescaler
@@ -121,4 +159,37 @@ void angleEst(void)
   GyX = Gx*GxConversionFactor;
   angleest = round(alpha*(angleest + dt*GyX) + (1-alpha)*roll); 
   
+}
+
+//MOTOR FUNCTIONS 
+void setup_Motor(struct Motor thisMotor) // sets up all motors in brake position
+{
+ // pinMode(thisMotor.enablePin, OUTPUT); // set PWM signal to output 
+  pinMode(thisMotor.inputPin1, OUTPUT); 
+  pinMode(thisMotor.inputPin2, OUTPUT); 
+  digitalWrite(thisMotor.inputPin1, LOW); 
+  digitalWrite(thisMotor.inputPin2, LOW); 
+  analogWrite(thisMotor.enablePin, 127); 
+
+}
+
+void goForward(struct Motor thisMotor)
+{
+  analogWrite(thisMotor.motorSpeed, 127); 
+  digitalWrite(thisMotor.inputPin1, HIGH); 
+  digitalWrite(thisMotor.inputPin2, LOW); 
+}
+
+void goBackward(struct Motor thisMotor)
+{
+  analogWrite(thisMotor.motorSpeed, 255); 
+  digitalWrite(thisMotor.inputPin1, LOW); 
+  digitalWrite(thisMotor.inputPin2, HIGH);  
+}
+
+void brakeMotor(struct Motor thisMotor)
+{
+  analogWrite(thisMotor.motorSpeed, 0); 
+  digitalWrite(thisMotor.inputPin1, LOW); 
+  digitalWrite(thisMotor.inputPin2, LOW);
 }
